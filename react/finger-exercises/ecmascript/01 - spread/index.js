b32b717620a1ab15f85e6e1275ef2ccb5b3fad73
Joaquin Battilana <battilanajn@gmail.com>
2019-03-29 16:54:22 -0300

    Fixed spread test 2, now if the arg is a list min returns the min of the list

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -2,6 +2,9 @@ import isArray from './utils';
 
 export function min(...args) {
   if (args.length === 1) {
+    if (isArray(args[0])) {
+      return Math.min(...args[0]);
+    }
     return args[0];
   }
   return Math.min();
