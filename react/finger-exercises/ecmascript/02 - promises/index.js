36318b573b14f743e89131d1298626c7c67ba12a
Joaquin Battilana <battilanajn@gmail.com>
2019-03-29 18:49:16 -0300

    Fixed promises test 2, now delay reject when time is more than 2000 ms

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,11 +1,17 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
+const MAXTIME = 2000;
+
 export function delay(time) {
   const startTime = Date.now();
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(Date.now() - startTime);
-    }, time);
+  return new Promise((resolve, reject) => {
+    if (time > MAXTIME) {
+      reject(new Error('This time is too much !'));
+    } else {
+      setTimeout(() => {
+        resolve(Date.now() - startTime);
+      }, time);
+    }
   });
 }
 
