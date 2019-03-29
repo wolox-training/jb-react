c69240b038dea8ef6582817346e23e0213a79688
Joaquin Battilana <battilanajn@gmail.com>
2019-03-29 16:48:53 -0300

    Fixed spread test 1, now min returns the param if is the only argument

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,7 +1,9 @@
 import isArray from './utils';
 
-export function min() {
-  isArray();
+export function min(...args) {
+  if (args.length === 1) {
+    return args[0];
+  }
   return Math.min();
 }
 
