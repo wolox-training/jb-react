5cfe2554404121e53f2ea41ceee1552942faf909
Joaquin Battilana <battilanajn@gmail.com>
2019-04-04 13:04:15 -0300

    Picking a key

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -56,7 +56,7 @@ class Game extends Component {
         'Go to move #' + move :
         'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={()=> this.jumpTo(move)}>{desc}</button>
           </li>
         );
