d641636dbf4bf14d2c5740581ac44bff50ae1e4d
Joaquin Battilana <battilanajn@gmail.com>
2019-04-05 13:35:51 -0300

    Now we use Fragment and import {Fragment} instead of using React.Fragment

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import Square from '../Square';
@@ -19,7 +19,7 @@ class Board extends Component {
 
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <div className={styles.boardRow}>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -35,7 +35,7 @@ class Board extends Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-      </React.Fragment>
+      </Fragment>
     );
   }
 }
