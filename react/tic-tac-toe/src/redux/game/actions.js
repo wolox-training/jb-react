05d2360785f164824c82f095e28cd81efad94368
Joaquin Battilana <battilanajn@gmail.com>
2019-04-12 16:01:57 -0300

    Needed commit for merging

--- react/tic-tac-toe/src/redux/game/actions.js
+++ react/tic-tac-toe/src/redux/game/actions.js
@@ -1,9 +1,12 @@
 export const actions = {
-
+    SQUARE_CLICK: "@@GAME/SQUARE_CLICK"
 }
 
 const actionCreators = {
-
+    squareClick: (squareId) => ({
+        type: actions.SQUARE_CLICK,
+        payload: squareId
+    })
 };
 
 export default actionCreators;
