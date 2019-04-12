3ca1edd3035ca828f9c22aaf9cd3507bfc998f5c
Joaquin Battilana <battilanajn@gmail.com>
2019-04-12 18:23:49 -0300

    Basic board now working

--- react/tic-tac-toe/src/redux/game/reducer.js
+++ react/tic-tac-toe/src/redux/game/reducer.js
@@ -3,7 +3,7 @@ import { calculateWinner } from '../../utils'
 
 const initialState = {
   history: [{ squares: Array(9).fill(null)}],
-  current: Array(9).fill(null),
+  current: { squares: Array(9).fill(null) } ,
   stepNumber: 0,
   xIsNext: true
 };
@@ -12,8 +12,7 @@ function reducer(state = initialState, action){
     switch (action.type) {
       case actions.SQUARE_CLICK:
         const history = state.history.slice(0, state.stepNumber + 1);
-        const current = history[history.length -1];
-        const squares = current.squares.slice();
+        const squares = state.current.squares.slice();
         if (calculateWinner(squares) || squares[action.payload]) {
           return state;
         }
@@ -22,6 +21,7 @@ function reducer(state = initialState, action){
           history: history.concat([{squares}]),
           stepNumber: history.length,
           xIsNext: !state.xIsNext,
+          current: { squares }
         }
       default:
         return state;
