79c45d637377972d2c18a0a2ae10b371790cfb08
Joaquin Battilana <battilanajn@gmail.com>
2019-04-12 14:24:34 -0300

    saved file

--- react/finger-exercises/redux/02 - react-redux/src/redux/cart/actions.js
+++ react/finger-exercises/redux/02 - react-redux/src/redux/cart/actions.js
@@ -1,5 +1,4 @@
 export const actions = {
-<<<<<<< HEAD
   ADD_TO_CART: '@@CART/ADD_TO_CART',
   ADD_ITEM: '@@CART/ADD_ITEM',
   REMOVE_ITEM: '@@CART/REMOVE_ITEM',
@@ -7,14 +6,6 @@ export const actions = {
 };
 
 const actionsCreators = {
-=======
-  ADD_TO_CART: '@@BOOK/ADD_TO_CART',
-  ADD_ITEM: '@@BOOK/ADD_ITEM',
-  REMOVE_ITEM: '@@BOOK/REMOVE_ITEM'
-};
-
-const actionCreators = {
->>>>>>> fae577276093df7414f1c493e659f63f057bca21
   addToCart: item => ({
     type: actions.ADD_TO_CART,
     payload: item
@@ -26,7 +17,6 @@ const actionCreators = {
   removeItem: itemId => ({
     type: actions.REMOVE_ITEM,
     payload: itemId
-<<<<<<< HEAD
   }),
   toggleCart: () => ({
     type: actions.TOGGLE_CART
@@ -34,9 +24,3 @@ const actionCreators = {
 };
 
 export default actionsCreators;
-=======
-  })
-};
-
-export default actionCreators;
->>>>>>> fae577276093df7414f1c493e659f63f057bca21
