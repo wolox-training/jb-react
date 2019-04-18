9782aec5f63ab23a20535e92a9bae42015653add
Joaquin Battilana <battilanajn@gmail.com>
2019-04-18 12:16:33 -0300

    Reducer logic is created

--- react/tic-tac-toe/src/redux/matches/actions.js
+++ react/tic-tac-toe/src/redux/matches/actions.js
@@ -1,9 +1,13 @@
+
+import MatchesService from "~services/MatchesService";
+
 export const actions = {
     GET_MATCHES = '@@MATCHES/GET_MATCHES'
 };
 
 const actionsCreators = {
-    getMatches: () => {
-        
-    }
+    getMatches: () => ({
+        payload: MatchesService.getMatches(),
+        action: actions.GET_MATCHES
+    })
 }
---
+++ react/tic-tac-toe/src/redux/matches/reducer.js
@@ -0,0 +1,15 @@
+import { actions } from './actions';
+
+const initialState = {
+    isLoading: false,
+    matchesHistory: []
+};
+
+function reducer(state = initialState, action) {
+    switch(action.type){
+        case actions.GET_MATCHES:
+
+        default:
+            return state;
+    }
+}
