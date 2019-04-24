347a6c7e4ad93d607b92d33669a8607da5062e02
Joaquin Battilana <battilanajn@gmail.com>
2019-04-24 13:47:41 -0300

    Fixed constant typo

--- react/tic-tac-toe/src/utils/index.js
+++ react/tic-tac-toe/src/utils/index.js
@@ -1,8 +1,8 @@
-import { lines } from '../constants';
+import { LINES } from '../constants';
 
 export const calculateWinner = squares => {
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let i = 0; i < LINES.length; i++) {
+    const [a, b, c] = LINES[i];
     if (squares[a] && 
     squares[a] === squares[b] && 
     squares[a] === squares[c]) {
