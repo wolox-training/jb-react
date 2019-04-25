ca737fd2e2a388a29bbabae6ee728a1e006479cb
Joaquin Battilana <battilanajn@gmail.com>
2019-04-25 15:41:03 -0300

    Changed email re to constants

--- react/tic-tac-toe/src/app/screens/Login/utils/validate/index.js
+++ react/tic-tac-toe/src/app/screens/Login/utils/validate/index.js
@@ -1,14 +1,7 @@
-
+import { EMAILRE } from '~constants';
 
 export const isRequired = value => value ? undefined : "Value is required";
 
 export const minLength = value => value.length >= 8 ? undefined : "Value should at least have 8 characters";
 
-export const validEmail = value => {
-    // this regular expression was found in internet, it's not tested hardly but it should work in normal cases
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(re.test(value)){
-        return undefined;
-    }
-    return "Email should be valid";
-};
+export const validEmail = value => EMAILRE.test(value) ? undefined : "Email should be valid";
