9565c063e9412561d73ef471ad02427c284c46ca
Joaquin Battilana <battilanajn@gmail.com>
2019-04-25 15:37:06 -0300

    Added commentary for email validate function

--- react/tic-tac-toe/src/app/screens/Login/utils/validate/index.js
+++ react/tic-tac-toe/src/app/screens/Login/utils/validate/index.js
@@ -5,6 +5,7 @@ export const isRequired = value => value ? undefined : "Value is required";
 export const minLength = value => value.length >= 8 ? undefined : "Value should at least have 8 characters";
 
 export const validEmail = value => {
+    // this regular expression was found in internet, it's not tested hardly but it should work in normal cases
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(re.test(value)){
         return undefined;
