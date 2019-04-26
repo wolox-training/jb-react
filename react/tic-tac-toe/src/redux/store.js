2fe6507ef3f92dad7d11323d7d934fe3bff5b07e
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-04-26 14:34:15 -0300

    Added redux thunk to middlewares

--- react/tic-tac-toe/src/redux/store.js
+++ react/tic-tac-toe/src/redux/store.js
@@ -2,7 +2,9 @@ import game from './game/reducer';
 import matches from './matches/reducer';
 import { reducer as form } from 'redux-form';
 
-import { createStore, compose, combineReducers} from 'redux';
+import { createStore, compose, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
+const enhancers = [applyMiddleware(thunk)]
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default createStore(combineReducers( { game, matches, form } ), composeEnhancers());
+export default createStore(combineReducers( { game, matches, form } ), composeEnhancers(...enhancers));
