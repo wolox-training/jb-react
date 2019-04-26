ba19c40066673de7ea1b63047a2b7eada455ce41
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-04-26 14:48:27 -0300

    Fixed indent problems

--- react/tic-tac-toe/src/constants/propTypes.js
+++ react/tic-tac-toe/src/constants/propTypes.js
@@ -1,9 +1,9 @@
 import { shape, string, number } from 'prop-types';
 
 export const matchPropType = shape ({
-    player_one: string,
-    player_two: string,
-    winner: string,
-    createdAt: string,
-    id: number
+  player_one: string,
+  player_two: string,
+  winner: string,
+  createdAt: string,
+  id: number
 });
--- react/tic-tac-toe/src/redux/game/actions.js
+++ react/tic-tac-toe/src/redux/game/actions.js
@@ -1,17 +1,17 @@
 export const actions = {
-    SQUARE_CLICK: '@@GAME/SQUARE_CLICK',
-    HISTORY_CLICK: '@@GAME/HISTORY_CLICK'
+  SQUARE_CLICK: '@@GAME/SQUARE_CLICK',
+  HISTORY_CLICK: '@@GAME/HISTORY_CLICK'
 };
 
 const actionsCreators = {
-    squareClick: squareId => ({
-        type: actions.SQUARE_CLICK,
-        payload: squareId
-    }),
-    historyClick: historyId => ({
-        type: actions.HISTORY_CLICK,
-        payload: historyId
-    }) 
+  squareClick: squareId => ({
+    type: actions.SQUARE_CLICK,
+    payload: squareId
+  }),
+  historyClick: historyId => ({
+    type: actions.HISTORY_CLICK,
+    payload: historyId
+  }) 
 };
 
 export default actionsCreators;
