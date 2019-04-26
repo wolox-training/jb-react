8102d054ff5c1bf14613e318d57b4c9f9dcd359b
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-04-26 15:54:01 -0300

    Added basic structure of login reducer and actioncreator

---
+++ react/tic-tac-toe/src/redux/login/reducer.js
@@ -0,0 +1,15 @@
+import actions from './actions';
+
+const initialState = {
+  isLoading: false,
+  hasError: false,
+  token: null
+}
+
+function reducer(state = initialState, action){
+  switch(action.type){
+
+  }
+}
+
+export default reducer;
