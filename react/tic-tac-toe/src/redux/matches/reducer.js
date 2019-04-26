fb439089e26ea3897168d034448e9ef58287e8c2
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-04-26 15:42:17 -0300

    mend
    Fixed merge problems

--- react/tic-tac-toe/src/redux/matches/reducer.js
+++ react/tic-tac-toe/src/redux/matches/reducer.js
@@ -1,15 +1,9 @@
 import { actions } from './actions';
 
 const initialState = {
-<<<<<<< HEAD
-    hasError: false,
-    isLoading: false,
-    matchesHistory: []
-=======
   hasError: false,
   isLoading: false,
   matchesHistory: []
->>>>>>> login-layout
 };
 
 function reducer(state = initialState, action) {
@@ -17,15 +11,11 @@ function reducer(state = initialState, action) {
     case actions.GET_MATCHES:
       return ({
         ...state,
-<<<<<<< HEAD
         isLoading: true
       });
     case actions.GET_MATCHES_SUCCESS:
       return ({
         ...state,
-=======
-        hasError: action.payload ? false : true,
->>>>>>> login-layout
         matchesHistory: action.payload,
         isLoading: false
       });
