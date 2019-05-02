6d97c961f025fcac2108be7c60784c213ef2c4cb
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-02 16:53:20 -0300

    Fixed indent problem

--- react/tic-tac-toe/src/app/screens/Login/index.js
+++ react/tic-tac-toe/src/app/screens/Login/index.js
@@ -3,11 +3,11 @@ import LoginFormContainer from './components/LoginForm';
 import styles from './styles.module.scss';
 
 function Login() {
-    return (
-      <div className={styles.loginView}>
-        <LoginFormContainer />
-      </div>
-    );
+  return (
+    <div className={styles.loginView}>
+      <LoginFormContainer />
+    </div>
+  );
 }
 
 export default Login;
