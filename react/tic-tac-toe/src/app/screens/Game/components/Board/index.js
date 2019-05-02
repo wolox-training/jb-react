eb9ff2f204ff40a37a7fa4e705f58aac63906210
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-02 15:15:13 -0300

    Now board is an stateless function component

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -4,31 +4,27 @@ import Square from '../Square';
 import { number } from 'prop-types';
 import styles from './styles.module.scss';
 
-class Board extends Component {
-  renderSquare = i => <Square id={i} />;
-
-  render() {
-    return (
-      <div className={styles.gameBoard}>
-        <div className={styles.boardRow}>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className={styles.boardRow}>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className={styles.boardRow}>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+const Board = () => {
+  return (
+    <div className={styles.gameBoard}>
+      <div className={styles.boardRow}>
+        <Square id={0} />
+        <Square id={1} />
+        <Square id={2} />
       </div>
-    );
-  }
-}
+      <div className={styles.boardRow}>
+        <Square id={3} />
+        <Square id={4} />
+        <Square id={5} />
+      </div>
+      <div className={styles.boardRow}>
+        <Square id={6} />
+        <Square id={7} />
+        <Square id={8} />
+      </div>
+    </div>
+  );
+};
 
 Board.propTypes = {
   id: number
