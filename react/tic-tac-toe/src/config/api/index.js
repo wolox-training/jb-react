5444468ee4630044fea30a5e741addfcfa608133
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-03 17:48:52 -0300

    Api now have token setup in headers when token is available

--- react/tic-tac-toe/src/config/api/index.js
+++ react/tic-tac-toe/src/config/api/index.js
@@ -1,8 +1,12 @@
 import { create } from 'apisauce';
 
+const headers = {
+  token: window.localStorage.getItem('token')
+};
 const api = create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 5000
+  timeout: 5000,
+  headers: { ...headers }
 });
 
 export default api;
