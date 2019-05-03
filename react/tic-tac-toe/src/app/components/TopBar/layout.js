f9b80dfa5d0bdf769735f4390b6a64d0f4f0c142
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-03 18:50:59 -0300

    Basic

--- react/tic-tac-toe/src/app/components/TopBar/layout.js
+++ react/tic-tac-toe/src/app/components/TopBar/layout.js
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 function TopBar() {
-  return <nav className={styles.navbar}> TopBAAAAAAAR </nav>;
+  return (
+    <nav className={styles.navbar}>
+      <img src="../../assets/wolox_logo.svg" className={styles.logo} />
+      <div className={styles.game}> Game </div>
+      <div> Game History </div>
+      <div> logout </div>
+    </nav>
+  );
 }
 
 export default TopBar;
