ad8a403b57d5509d76ecc262c1c03f9dc72da32c
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-03 18:17:14 -0300

    Added some assets for logos and now topbar is rendering

--- react/tic-tac-toe/src/app/components/TopBar/index.js
+++ react/tic-tac-toe/src/app/components/TopBar/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TopBar from './layout';
 
 class TopBarContainer extends Component {
   render() {
--- react/tic-tac-toe/src/app/index.js
+++ react/tic-tac-toe/src/app/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Game from './screens/Game';
 import Login from './screens/Login';
 import { history } from '~redux/store';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import ConditionalRoute from './components/ConditionalRoute';
 import { isLogged, isNotLogged } from './utils/validate';
+import TopBarContainer from './components/TopBar';
 
 import '../scss/application.scss';
 import ConditionalRedirect from './components/ConditionalRedirect';
@@ -15,6 +16,7 @@ function App() {
   return (
     <ConnectedRouter history={history}>
       <Switch>
+        <Route strict path="/topbar" component={TopBarContainer} />
         <ConditionalRoute strict path="/game" component={Game} condition={isLogged} redirect="/login" />
         <ConditionalRoute strict path="/login" component={Login} condition={isNotLogged} redirect="/game" />
         <ConditionalRedirect condition={isLogged} first="/game" second="/login" />
