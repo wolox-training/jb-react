3aa0637e83a82d836520c96d15f74311b99a3773
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-07 12:22:49 -0300

    Imported logo

--- react/tic-tac-toe/src/app/screens/Main/components/TopBar/layout.js
+++ react/tic-tac-toe/src/app/screens/Main/components/TopBar/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import logo from '../../../../assets/wolox_logo.svg';
 
 function TopBar() {
   return (
     <nav className={styles.navbar}>
-      <img src="../../assets/wolox_logo.svg" className={styles.logo} />
+      <img src={logo} className={styles.logo} />
       <div className={styles.game}> Game </div>
       <div> Game History </div>
       <div> logout </div>
