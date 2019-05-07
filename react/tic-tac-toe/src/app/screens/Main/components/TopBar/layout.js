caedc7a0f0367fb258211a13aa9308b7cc774540
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-07 13:07:27 -0300

    Now topbar have color and text is white

--- react/tic-tac-toe/src/app/screens/Main/components/TopBar/layout.js
+++ react/tic-tac-toe/src/app/screens/Main/components/TopBar/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import logo from '../../../../assets/wolox_logo.svg';
+import logo from '../../../../assets/wolox_logo_white.svg';
 
 function TopBar() {
   return (
