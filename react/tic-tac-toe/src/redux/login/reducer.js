b3fdb82cb3e595068433c35e0df06da9fb0cb0f1
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-09 15:45:30 -0300

    Used completeState for async states

--- react/tic-tac-toe/src/redux/login/reducer.js
+++ react/tic-tac-toe/src/redux/login/reducer.js
@@ -1,28 +1,30 @@
 import { actions } from './actions';
+import { completeState } from 'redux-recompose';
 
-const initialState = {
-  isLoading: false,
-  hasError: false
+const initialStateDescription = {
+  token: null
 };
+
+const initialState = completeState(initialStateDescription);
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case actions.LOGIN:
       return {
         ...state,
-        isLoading: true
+        tokenLoading: true
       };
     case actions.LOGIN_SUCCESS:
       return {
         ...state,
-        isLoading: false,
-        hasError: false
+        tokenLoading: false,
+        tokenError: false
       };
     case actions.LOGIN_FAIL:
       return {
         ...state,
-        isLoading: false,
-        hasError: true
+        tokenLoading: false,
+        tokenError: true
       };
     default:
       return state;
--- react/tic-tac-toe/src/redux/matches/reducer.js
+++ react/tic-tac-toe/src/redux/matches/reducer.js
@@ -1,29 +1,30 @@
 import { actions } from './actions';
+import { completeState } from 'redux-recompose';
 
-const initialState = {
-  hasError: false,
-  isLoading: false,
+const initialStateDescription = {
   matchesHistory: []
 };
+
+const initialState = completeState(initialStateDescription);
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case actions.GET_MATCHES:
       return {
         ...state,
-        isLoading: true
+        matchesHistoryLoading: true
       };
     case actions.GET_MATCHES_SUCCESS:
       return {
         ...state,
         matchesHistory: action.payload,
-        isLoading: false
+        matchesHistoryLoading: false
       };
     case actions.GET_MATCHES_FAIL:
       return {
         ...state,
-        isLoading: false,
-        hasError: true
+        matchesHistoryLoading: false,
+        matchesHistoryError: true
       };
     default:
       return state;
