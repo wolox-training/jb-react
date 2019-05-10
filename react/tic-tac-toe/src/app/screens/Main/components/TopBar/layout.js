6fbc50c2c5732303cff0ba6a632aad3fa0cb38bd
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-10 17:42:20 -0300

    Created buttons constants for text and icon

--- react/tic-tac-toe/src/app/screens/Main/components/TopBar/layout.js
+++ react/tic-tac-toe/src/app/screens/Main/components/TopBar/layout.js
@@ -2,14 +2,23 @@ import React from 'react';
 import styles from './styles.module.scss';
 import logo from '../../../../assets/wolox_logo_white.svg';
 import TopBarButton from '../TopBarButton';
+import { BUTTONS } from '../constants';
 
 function TopBar({ onLogoutClick, onGameClick, onGameHistoryClick }) {
   return (
     <nav className={styles.navbar}>
       <img src={logo} className={styles.logo} />
-      <TopBarButton label="Game" handleClick={onGameClick} buttonClass="fas fa-gamepad" />
-      <TopBarButton label="Match History" handleClick={onGameHistoryClick} buttonClass="fas fa-history" />
-      <TopBarButton label="Logout" handleClick={onLogoutClick} buttonClass="fas fa-sign-out-alt" />
+      <TopBarButton label={BUTTONS.GAME.label} handleClick={onGameClick} buttonClass={BUTTONS.GAME.icon} />
+      <TopBarButton
+        label={BUTTONS.MATCH_HISTORY.label}
+        handleClick={onGameHistoryClick}
+        buttonClass={BUTTONS.MATCH_HISTORY.icon}
+      />
+      <TopBarButton
+        label={BUTTONS.LOGOUT.label}
+        handleClick={onLogoutClick}
+        buttonClass={BUTTONS.LOGOUT.icon}
+      />
     </nav>
   );
 }
