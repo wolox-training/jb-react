d3442dfcd345883e52c5e51b313e25417992454e
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-16 14:35:08 -0300

    Fixed test 'pow returns the power based on two numeric arguments'

--- react/finger-exercises/testing_1/01 - matchers/test.js
+++ react/finger-exercises/testing_1/01 - matchers/test.js
@@ -1,8 +1,9 @@
 import pow from '.';
 
 describe('01 - matchers', () => {
-  xit('pow returns the power based on two numeric arguments', () => {
-    pow();
+  it('pow returns the power based on two numeric arguments', () => {
+    expect(pow(4, 2)).toEqual(16);
+    expect(pow(10, 3)).toEqual(1000);
   });
   xit('pow returns undefined if there is no arguments', () => {
   });
