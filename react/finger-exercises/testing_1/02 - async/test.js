29608917f158499be358136b0c0df4493c19112e
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-16 15:33:52 -0300

    Fixed error promises tests

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -2,13 +2,19 @@ import getData from '.';
 
 describe('02 - async', () => {
   it('getData resolves the data if true is sent as argument (use async/await)', async () => {
+    expect.assertions(1);
     await expect(getData(true)).resolves.toMatch('data');
   });
   it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+    expect.assertions(1);
     return expect(getData(true)).resolves.toMatch('data');
   });
-  xit('getData throws error if false is sent as argument (use async/await)', async () => {
+  it('getData throws error if false is sent as argument (use async/await)', async () => {
+    expect.assertions(1);
+    await expect(getData(false)).rejects.toThrow(Error);
   });
-  xit('getData throws error if false is sent as argument (avoid async/await)', () => {
+  it('getData throws error if false is sent as argument (avoid async/await)', () => {
+    expect.assertions(1);
+    return expect(getData(false)).rejects.toThrow(Error);
   });
 });
