bd2bfd98857455c3756677256f6c8bbe2f53e90a
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-16 15:13:37 -0300

    fixed test 'getData resolves thedata if true is sent as argument avoiding async await'

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -2,9 +2,10 @@ import getData from '.';
 
 describe('02 - async', () => {
   it('getData resolves the data if true is sent as argument (use async/await)', async () => {
-    await expect(getData(true)).resolves.toBe('data');
+    await expect(getData(true)).resolves.toMatch('data');
   });
-  xit('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+  it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+    return expect(getData(true)).resolves.toMatch('data');
   });
   xit('getData throws error if false is sent as argument (use async/await)', async () => {
   });
