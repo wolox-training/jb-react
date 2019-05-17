bbbf0eb46550b65b4078e46eaf7ba21954569158
Joaquin Battilana <jbattilana@itba.edu.ar>
2019-05-17 12:22:12 -0300

    doing

--- react/finger-exercises/testing_2/src/App.test.js
+++ react/finger-exercises/testing_2/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -8,7 +8,9 @@ import App from './App';
 
 configure({ adapter: new Adapter() });
 describe('App', () => {
-  xit('renders without crashing', () => {
+  it('renders without crashing', () => {
+    const tree = shallow(<App />);
+    expect(tree.find('#root').children().length).toBe(1);
   });
 
   xit('matches the snapshot', () => {
